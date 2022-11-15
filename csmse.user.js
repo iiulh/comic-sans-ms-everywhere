4eb435874cd91a2fe873d14c2efbf0b82183e36b
Shaehl <38476717+Shaehl@users.noreply.github.com>
2022-11-15 21:17:41 +0300

    "font-family" value, shorter style tag appending

--- csmse.user.js
+++ csmse.user.js
@@ -1,12 +1,21 @@
 // ==UserScript==
-// @version     1.0.4
+// @version     1.0.5
+// @run-at      document-body
+// @noframes
 // @name        Comic Sans MS everywhere
 // @description Changes font to Comic Sans MS where it's possible.
 // @author      Shaehl
 // @namespace   https://github.com/Shaehl
 // @match       *://*/*
+// @grant GM_getValue
+// @grant GM_setValue
 // ==/UserScript==
 
-const csStyle = document.createElement("style")
-csStyle.innerHTML = '*{font-family:"Comic Sans MS" !important;}'
-document.head.appendChild(csStyle)
+let fontFamily = GM_getValue("fontFamily")
+if (fontFamily == undefined) {
+  fontFamily = '"Comic Sans MS", "Comic Sans", cursive'
+  GM_setValue("fontFamily", fontFamily)
+  // i wish i knew a shorter way to do this
+}
+
+document.head.innerHTML += `\n<style>*{font-family: ${fontFamily} !important;}</style>`
